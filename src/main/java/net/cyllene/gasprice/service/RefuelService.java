dba0b885fd0908218de48ac8a2b090ce4ebc4a1b
Mikhail Antonov <hermes@cyllene.net>
2021-02-01 17:45:23 +0300

    Lazy fetch of referenced fields

--- src/main/java/net/cyllene/gasprice/service/RefuelService.java
+++ src/main/java/net/cyllene/gasprice/service/RefuelService.java
@@ -2,20 +2,20 @@ package net.cyllene.gasprice.service;
 
 import lombok.RequiredArgsConstructor;
 import net.cyllene.gasprice.dto.RefuelDto;
+import net.cyllene.gasprice.model.Car;
+import net.cyllene.gasprice.model.Petrol;
 import net.cyllene.gasprice.model.Refuel;
-import net.cyllene.gasprice.repository.CarRepository;
-import net.cyllene.gasprice.repository.PetrolRepository;
 import net.cyllene.gasprice.repository.RefuelRepository;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RefuelService {
     private final RefuelRepository refuelRepository;
-    private final CarRepository carRepository;
-    private final PetrolRepository petrolRepository;
+    private final EntityManager entityManager;
 
     public List<Refuel> listForSummary() {
         return refuelRepository.findAllByOrderByDatetimeDesc();
@@ -25,8 +25,8 @@ public class RefuelService {
         Refuel refuel = new Refuel();
         refuel.setId(dto.getId() == null || dto.getId().equals(0) ? null : dto.getId());
         refuel.setDatetime(dto.getDatetime());
-        refuel.setCar(carRepository.findById(dto.getCar().getId()).orElseThrow());
-        refuel.setPetrol(petrolRepository.findById(dto.getPetrol().getId()).orElseThrow());
+        refuel.setCar(entityManager.getReference(Car.class, dto.getCar().getId()));
+        refuel.setPetrol(entityManager.getReference(Petrol.class, dto.getPetrol().getId()));
         refuel.setPrice(dto.getPrice());
         refuel.setAmount(dto.getAmount());
         refuel.setMileage(dto.getMileage());
